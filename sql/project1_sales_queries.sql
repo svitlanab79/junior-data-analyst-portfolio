76500c257c7e23a8330bc4e2525675a17dceaed5
Svitlana Basurina <study.gane@gmail.com>
2026-02-02 23:01:23 +0100

    Project 2: Creating Tables and Exploring DB

--- sql/project1_sales_queries.sql
+++
@@ -1,8 +0,0 @@
-WITH monthly_sales AS (
-  SELECT strftime('%Y-%m', OrderDate) AS Month, SUM(Revenue) AS TotalRevenue
-  FROM Orders
-  GROUP BY 1
-)
-SELECT Month, TotalRevenue,
-       LAG(TotalRevenue) OVER(ORDER BY Month) AS PrevMonthRevenue
-FROM monthly_sales;
